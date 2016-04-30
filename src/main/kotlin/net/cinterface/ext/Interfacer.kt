ef1fb3abe84e9a2b47b9cfeb5f42470b81a4a8a3
PizzaCrust <torchprojectserver@gmail.com>
2016-04-30 22:56:29 +0700

    impl

--- src/main/kotlin/net/cinterface/ext/Interfacer.kt
+++ src/main/kotlin/net/cinterface/ext/Interfacer.kt
@@ -1,16 +1,17 @@
 package net.cinterface.ext
 
 import javassist.CtClass
+import java.io.File
 
 /**
  * Represents a class that converts classes into interfaces.
  *
  * @since 1.0-SNAPSHOT
- * @authorPizzaCrust
+ * @author PizzaCrust
  */
 interface Interfacer {
     /**
      * This method is called when a .class is converted into a CtClass intending to be converted to a interface.
      */
-    fun convert(theClass: CtClass)
+    fun convert(theClass: CtClass, outputClassFile: File)
 }
---
+++ src/main/kotlin/net/cinterface/impl/ClassInterfacer.kt
@@ -0,0 +1,26 @@
+package net.cinterface.impl
+
+import javassist.ClassPool
+import javassist.CtClass
+import javassist.CtNewMethod
+import net.cinterface.ext.Interfacer
+import java.io.DataOutputStream
+import java.io.File
+import java.io.FileOutputStream
+
+/**
+ * The implementation of the Interfacer interface.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+class ClassInterfacer : Interfacer {
+    override fun convert(theClass: CtClass, outputClassFile: File) {
+        val newClass: CtClass = ClassPool.getDefault().makeClass(theClass.name + "I")
+        for (method in theClass.declaredMethods) {
+            newClass.addMethod(CtNewMethod.abstractMethod(method.returnType, method.name, method.parameterTypes, arrayOf(), newClass))
+        }
+        val dataStream: DataOutputStream = DataOutputStream(FileOutputStream(outputClassFile))
+        newClass.classFile.write(dataStream)
+    }
+}
